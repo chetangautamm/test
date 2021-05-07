c1e61ea70aaddbc753869d23329dd75beb2d9a9a
Ubuntu <k8suser@Host.rmgavuyqwnmuzcdntdbxudk2he.rx.internal.cloudapp.net>
2021-05-07 08:57:52 +0000

    Latest Commit

--- k8s-cleanup.sh
+++ k8s-cleanup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#Capture Namespace
+namespace=$(kubectl get ns | awk 'NR==2{print $1}');
+
 #Getting all deployments in OSM's namespace
-kubectl get deployments -n d81d0c8e-dde0-464d-930c-79de5183b4db
+kubectl get deployments -n $namespace
 
 #Deleting all deployments in Osm's namespace
-kubectl delete deployments -n d81d0c8e-dde0-464d-930c-79de5183b4db --all
+kubectl delete deployments -n $namespace --all
 
 #Validating the deletion
-kubectl get deployments -n d81d0c8e-dde0-464d-930c-79de5183b4db
+kubectl get deployments -n $namespace
