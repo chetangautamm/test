258e97c75570c43d00a5fa3dcdae950dc8ffe964
Ubuntu <k8suser@Host.rmgavuyqwnmuzcdntdbxudk2he.rx.internal.cloudapp.net>
2021-05-07 09:31:23 +0000

    latest commit

--- osm-k8s-validate.sh
+++ osm-k8s-validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-var1=$(osm k8scluster-list | grep  -P '(^|\s)\K(kubespray-cluster?=\s|$)'| awk '{print $2}');
+var1=$(osm k8scluster-list | grep  -P '(^|\s)\Kkubespray-cluster?=\s|$)'| awk '{print $2}');
 var2="kubespray-cluster"
 if [ "$var1" = "$var2" ]
 then
