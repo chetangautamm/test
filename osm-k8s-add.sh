13b6639ec5795b6dc9c3091922c8ad8aca5156a7
Ubuntu <k8suser@Host.rmgavuyqwnmuzcdntdbxudk2he.rx.internal.cloudapp.net>
2021-05-24 08:32:08 +0000

    osm-k8s-add.sh edited

--- osm-k8s-add.sh
+++ osm-k8s-add.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-#Adding Kubernetes Cluster
-osm k8scluster-add --creds kubespray-config.yaml --version '1.20.2' --vim OpenstackR-1 --description "Kubespray Cluster" --k8s-nets '{"net1": "external"}' kubespray-cluster
+var1=$(osm k8scluster-list | grep -P '(^|\s)\Kkubespray-cluster(?=\s|$)' | awk '{print $2}');
+var2="kubespray-cluster"
+if [ "$var1" = "$var2" ]
+then
+   echo "Kubernetes Cluster is Already Present!!"
+   osm k8scluster-list
+else
+   osm k8scluster-add --creds kubespray-config.yaml --version '1.20.2' --vim OpenstackR-1 --description "Kubespray Cluster" --k8s-nets '{"net1": "external"}' kubespray-   cluster
+   sleep 20	  
+   echo "Kubernetes Cluster is Added!!"
+fi
 
-sleep 20
+#Adding Kubernetes Cluster
+#osm k8scluster-add --creds kubespray-config.yaml --version '1.20.2' --vim OpenstackR-1 --description "Kubespray Cluster" --k8s-nets '{"net1": "external"}' kubespray-cluster
+
+#sleep 20
