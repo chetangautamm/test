2ef3a42319c22b99ffd8efefe88597fccf8b11ce
Ubuntu <k8suser@Host.rmgavuyqwnmuzcdntdbxudk2he.rx.internal.cloudapp.net>
2021-06-11 09:47:48 +0000

    Api-fication

---
+++ Get_requests.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[350]:
+
+
+import requests
+import json
+import sys
+import logging
+import os
+import time
+import urllib3
+
+#Token Generation
+url = "https://20.198.88.127:9999/osm"
+def get_token(url):
+    payload = json.dumps({ "username": "admin", "password": "admin", "project_id": "admin" })
+    headers = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    r1 = requests.request("POST", url, headers=headers, data=payload, verify=False)
+    if r1.status_code == 200:
+        token = r1.json()['id']
+        return token
+    else:
+        print("Failed to get token.")   
+
+token = get_token(url+"/admin/v1/tokens")
+print(f"token: {token}")
+
+
+# In[351]:
+
+
+#Checking VIM Accounts
+def get_vim1_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    vims = response.json()
+    for vim in vims:
+        if vim['name'] == "OpenstackR-1":
+            return vim['_id']  
+
+def get_vim2_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    vims = response.json()
+    for vim in vims:
+        if vim['name'] == "OpenstackR-2":
+            return vim['_id']  
+        
+vimId1 = get_vim1_id(url+f"/admin/v1/vims", token)
+print(f"vimAccountId: {vimId1}")
+
+
+vimId2 = get_vim2_id(url+f"/admin/v1/vims", token)
+print(f"vimAccountId: {vimId2}")
+
+
+# In[352]:
+
+
+def get_opensips_ns_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for opensips in data:
+        if opensips['name'] == "opensips":
+            return opensips['name'],opensips['_id'] 
+
+def get_uas_ns_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uas in data:
+        if uas['name'] == "uas":
+            return uas['name'],uas['_id'] 
+    
+def get_uac_ns_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uac in data:
+        if uac['name'] == "uac":
+            return uac['name'], uac['_id'] 
+
+nsopensips = get_opensips_ns_id(url+f"/nslcm/v1/ns_instances_content", token)
+print(f"ns_list: {nsopensips}")
+
+nsuas = get_uas_ns_id(url+f"/nslcm/v1/ns_instances_content", token)
+print(f"ns_list: {nsuas}")
+
+nsuac = get_uac_ns_id(url+f"/nslcm/v1/ns_instances_content", token)
+print(f"ns_list: {nsuac}")
+
+
+
+# In[353]:
+
+
+def get_instance_name(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for instance in data:
+        print(instance['name'], instance['_id'])
+nam = get_instance_name(url+f"/nslcm/v1/ns_instances_content", token)
+
+
+# In[354]:
+
+
+#Helm Repo 
+def get_helm_repo(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for helm in data:
+        if helm['name'] == "helm-osm":
+            return helm['name'],helm['_id']   
+
+repo = get_helm_repo(url+f"/admin/v1/k8srepos", token)
+print(f"repo_name: {repo}")
+
+
+# In[355]:
+
+
+#K8sclusters
+def get_k8s_cluster(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for k8s in data:
+        if k8s['name'] == "kubespray-cluster":
+            return k8s['name'],k8s['_id']  
+
+def get_kubeadm_cluster(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for kube in data:
+        if kube['name'] == "kubeadm-cluster":
+            return kube['name'],kube['_id']   
+
+cluster1 = get_k8s_cluster(url+f"/admin/v1/k8sclusters", token)
+print(f"cluster_name: {cluster1}")
+
+cluster2 = get_kubeadm_cluster(url+f"/admin/v1/k8sclusters", token)
+print(f"cluster_name: {cluster2}")
+
+
+# In[356]:
+
+
+#NSD List
+def get_opensips_nsd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for opensips in data:
+        if opensips['name'] == "jenkins_opensips-7_ns":
+            return opensips['name'],opensips['_id']   
+
+def get_uas_nsd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uas in data:
+        if uas['name'] == "jenkins_uas-7_ns":
+            return uas['name'],uas['_id']   
+
+def get_uac_nsd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uac in data:
+        if uac['name'] == "jenkins_uac-7_ns":
+            return uac['name'],uac['_id']   
+
+nsdname = get_opensips_nsd(url+f"/nsd/v1/ns_descriptors", token)
+print(f"nsd_name: {nsdname}")
+
+
+nsdname = get_uas_nsd(url+f"/nsd/v1/ns_descriptors", token)
+print(f"nsd_name: {nsdname}")
+
+
+nsdname = get_uac_nsd(url+f"/nsd/v1/ns_descriptors", token)
+print(f"nsd_name: {nsdname}")
+
+
+# In[357]:
+
+
+#VNFD List
+def get_opensips_vnfd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for opensips in data:
+        if opensips['id'] == "jenkins_opensips-7_knf":
+            return opensips['id'], opensips['_id']   
+
+def get_uas_vnfd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uas in data:
+        if uas['id'] == "jenkins_uas-7_knf":
+            return uas['id'], uas['_id']   
+
+def get_uac_vnfd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uac in data:
+        if uac['id'] == "jenkins_uac-7_knf":
+            return uac['id'], uac['_id']   
+
+vnfdname = get_opensips_vnfd(url+f"/vnfpkgm/v1/vnf_packages", token)
+print(f"nsd_name: {vnfdname}")
+
+
+vnfdname = get_uas_vnfd(url+f"/vnfpkgm/v1/vnf_packages", token)
+print(f"nsd_name: {vnfdname}")
+
+
+vnfdname = get_uac_vnfd(url+f"/vnfpkgm/v1/vnf_packages", token)
+print(f"nsd_name: {vnfdname}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ cleanup_osm.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import requests
+import json
+import sys
+import logging
+import os
+import time
+import urllib3
+
+#Token Generation
+url = "https://52.172.254.152:9999/osm"
+def get_token(url):
+    payload = json.dumps({ "username": "admin", "password": "admin", "project_id": "admin" })
+    headers = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+    r1 = requests.request("POST", url, headers=headers, data=payload, verify=False)
+    if r1.status_code == 200:
+        token = r1.json()['id']
+        return token
+    else:
+        print("Failed to get token.")   
+
+token = get_token(url+"/admin/v1/tokens")
+print(f"token: {token}")
+urllib3.disable_warnings()
+
+
+# In[13]:
+
+
+#Fetching NS Instance Ids
+def get_opensips_ns_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for opensips in data:
+        if opensips['name'] == "opensips":
+            return opensips['_id'] 
+
+def get_uas_ns_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uas in data:
+        if uas['name'] == "uas":
+            return uas['_id'] 
+    
+def get_uac_ns_id(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uac in data:
+        if uac['name'] == "uac":
+            return uac['_id']
+
+opensipsId = get_opensips_ns_id(url+f"/nslcm/v1/ns_instances_content", token)
+print(f"opensipsId: {opensipsId}")
+
+uasId = get_uas_ns_id(url+f"/nslcm/v1/ns_instances_content", token)
+print(f"uasId: {uasId}")
+
+uacId = get_uac_ns_id(url+f"/nslcm/v1/ns_instances_content", token)
+print(f"uacId: {uacId}")
+
+
+# In[14]:
+
+
+#Deleting NS Instances
+def delete_opensips_ns_instance(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("NS instance resource deleted successfully.")
+delete_opensips_ns_instance(token, url+f"/nslcm/v1/ns_instances_content/{opensipsId}")
+
+def delete_uas_ns_instance(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("NS instance resource deleted successfully.")
+delete_uas_ns_instance(token, url+f"/nslcm/v1/ns_instances_content/{uasId}")
+
+def delete_uac_ns_instance(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("NS instance resource deleted successfully.")
+delete_uac_ns_instance(token, url+f"/nslcm/v1/ns_instances_content/{uacId}")
+
+time.sleep(30)
+
+
+# In[15]:
+
+
+#NSD List
+def get_opensips_nsd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for opensips in data:
+        if opensips['name'] == "jenkins_opensips-7_ns":
+            return opensips['_id']   
+
+def get_uas_nsd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uas in data:
+        if uas['name'] == "jenkins_uas-7_ns":
+            return uas['_id']   
+
+def get_uac_nsd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uac in data:
+        if uac['name'] == "jenkins_uac-7_ns":
+            return uac['_id']   
+
+opensipsNsdId = get_opensips_nsd(url+f"/nsd/v1/ns_descriptors", token)
+print(f"opensipsNsdId: {opensipsNsdId}")
+
+uasNsdId = get_uas_nsd(url+f"/nsd/v1/ns_descriptors", token)
+print(f"uasNsdId: {uasNsdId}")
+
+uacNsdId = get_uac_nsd(url+f"/nsd/v1/ns_descriptors", token)
+print(f"uacNsdId: {uacNsdId}")
+
+
+# In[16]:
+
+
+#Deleting nsd
+def delete_opensips_nsd(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("Opensips nsd deleted successfully.")
+delete_opensips_nsd(token, url+f"/nsd/v1/ns_descriptors/{opensipsNsdId}")
+
+def delete_uas_nsd(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("Uas nsd deleted successfully.")
+delete_uas_nsd(token, url+f"/nsd/v1/ns_descriptors/{uasNsdId}")
+
+def delete_uac_nsd(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("Uac nsd deleted successfully.")
+delete_uac_nsd(token, url+f"/nsd/v1/ns_descriptors/{uacNsdId}")
+
+
+# In[17]:
+
+
+#VNFD List
+def get_opensips_vnfd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for opensips in data:
+        if opensips['id'] == "jenkins_opensips-7_knf":
+            return opensips['_id']   
+
+def get_uas_vnfd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uas in data:
+        if uas['id'] == "jenkins_uas-7_knf":
+            return uas['_id']   
+
+def get_uac_vnfd(url, token):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("GET", url, headers=headers, verify=False)
+    data = response.json()
+    for uac in data:
+        if uac['id'] == "jenkins_uac-7_knf":
+            return uac['_id']   
+
+opensipsVnfdId = get_opensips_vnfd(url+f"/vnfpkgm/v1/vnf_packages", token)
+print(f"opensipsVnfdId: {opensipsVnfdId}")
+
+
+uasVnfdId = get_uas_vnfd(url+f"/vnfpkgm/v1/vnf_packages", token)
+print(f"uasVnfdId: {uasVnfdId}")
+
+
+uacVnfdId = get_uac_vnfd(url+f"/vnfpkgm/v1/vnf_packages", token)
+print(f"uacVnfdId: {uacVnfdId}")
+
+
+# In[18]:
+
+
+#Deleting vnfd
+def delete_opensips_vnfd(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("Opensips vnfd deleted successfully.")
+delete_opensips_vnfd(token, url+f"/vnfpkgm/v1/vnf_packages/{opensipsVnfdId}")
+
+def delete_uas_vnfd(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("Uas vnfd deleted successfully.")
+delete_uas_vnfd(token, url+f"/vnfpkgm/v1/vnf_packages/{uasVnfdId}")
+
+def delete_uac_vnfd(token, url):
+    headers = { 'Authorization': f'Bearer {token}', 'Accept': 'application/json' }
+    response = requests.request("DELETE", url, headers=headers, verify=False )
+    if response.status_code == 204:
+        print("Uac vnfd deleted successfully.")
+delete_uac_vnfd(token, url+f"/vnfpkgm/v1/vnf_packages/{uacVnfdId}")
+
+
+# In[ ]:
+
+
+
+
